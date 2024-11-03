95c2e2938c33033ef92a220d304e9c69f93a26ed
hikaru <hika1785040525@gmail.com>
2024-11-03 20:38:33 +0900

    CORS設定を有効に設定

---
+++ src/main/java/com/example/demo/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 全てのエンドポイントに対して
+                .allowedOrigins("http://localhost:3000") // ReactアプリのURLを指定
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 許可するHTTPメソッド
+                .allowedHeaders("*"); // 許可するHTTPヘッダー
+    }
+}
